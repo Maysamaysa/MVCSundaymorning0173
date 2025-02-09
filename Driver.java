c14921f6c5b22118cf8b805aa5f8f78cc0d79cc7
Bocchi <139888492+Maysamaysa@users.noreply.github.com>
2025-02-09 11:57:49 +0700

    Add files via upload
    view to display gui stuff Driver to jusr run (main) entry point,landing etc.

---
+++ Driver.java
@@ -0,0 +1,9 @@
+//driver start Main
+import java.io.IOException;
+public class Driver {
+
+    public static void main(String[] args) throws IOException{
+        View view = new View();
+        new Con(view);
+    }
+}
---
+++ View.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+//view แสดงผลหนเาจอต่างๆผ
+public class View {
+    JFrame frame;
+    JTextField idinput;
+    JButton checkb;
+    JLabel resultLabel;
+    JLabel type;
+    public View() {
+        frame = new JFrame("MVC Sunday ข้อ 2");
+        frame.setSize(500, 250);
+        frame.setLayout(new GridLayout(5, 1));
+        frame.setLocationRelativeTo(null);
+        idinput = new JTextField();
+        type = new JLabel("Suit Type: -", SwingConstants.CENTER);
+        checkb = new JButton("checking");
+        resultLabel = new JLabel("Id?", SwingConstants.CENTER);
+        frame.add(new JLabel("Id :"));
+        frame.add(idinput);
+        frame.add(checkb);
+        frame.add(resultLabel);
+        frame.add(type);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+//for controller
+    public String getSuitId() {
+        return idinput.getText().trim();
+    }
+
+    public void setResult(String message) {
+        resultLabel.setText(message);
+    }
+
+    public void setSuitType(String type1) {
+        type.setText("Suit Type: " + type1);
+    }
+
+    public JButton getcheckb() { return checkb; }
+}
